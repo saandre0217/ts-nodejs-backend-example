b93bdbecd1ebeb6ad36f0a38db8abfd538adc34a
Sydney Andre <s.a.andre0217@gmail.com>
2024-06-06 16:16:48 -0500

    added log to check env

--- server/index.ts
+++ server/index.ts
@@ -16,5 +16,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port} dbname: ${process.env.Database}`);
 });
